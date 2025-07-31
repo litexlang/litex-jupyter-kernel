b26094f8439b8118fa1f6cdf4426754922acc72c
kidhero <scanfandprintf@icloud.com>
2025-07-31 13:51:58 +0800

    Remove build_and_upload.sh script as it is no longer needed

--- build_and_upload.sh
+++
@@ -1,7 +0,0 @@
-# ensure your python env is target env
-# ensure edited pyproject.toml file
-# ensure you have build and twine installed
-# ensure configurated your ~/.pypirc file
-rm -rf dist
-python3 -m build
-python3 -m twine upload --repository pypi dist/*
